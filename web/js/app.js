9b6102602858acae97ebeafb67f5331385a97428
Bino <ivance.kevin@gmail.com>
2015-04-15 17:18:30 +0200

    Upload wav

--- web/js/app.js
+++ web/js/app.js
@@ -37,8 +37,8 @@
                         // send Blob objects via XHR requests:
                         disableImageResize: /Android(?!.*Chrome)|Opera/
                             .test(window.navigator.userAgent),
-                        maxFileSize: 5000000,
-                        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
+                        maxFileSize: 52428800,
+                        acceptFileTypes: /(\.|\/)(gif|jpe?g|png|wav)$/i
                     });
                 }
             }
