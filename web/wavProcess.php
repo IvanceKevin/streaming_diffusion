21e6473e6d2ed76a3eefad2e198620c25a5580c9
tpuser <tpuser@mamba03.univ-lr.fr>
2015-04-15 16:15:19 +0200

    Zendsearch

---
+++ web/wavProcess.php
@@ -0,0 +1,39 @@
+<?php
+
+function wavProcess($index, $doc, $documentPath) {
+
+	$fileName = basename($documentPath);
+	$fileNameField = \ZendSearch\Lucene\Document\Field::text(
+		'filename',
+		$fileName
+	);
+	
+	//APPEL DU FICHIER PYTHON
+	exec('python ../xmpParse.py',$res, $retcode);
+
+	// Title	
+	$title = $res[0];
+	$titleField = \ZendSearch\Lucene\Document\Field::text(
+		'title',
+		$title
+	);
+
+	// echo "Title : " . $title[0] . PHP_EOL;
+
+	// Subject
+	for ($i = 1; $i < count($res); $i++) {
+		$subject = $res[$i];
+		$subjectField = \ZendSearch\Lucene\Document\Field::text(
+			$subject,
+			$subject
+		);
+		$doc->addField($subjectField);
+	}
+
+	// echo "Subject : " . $subject[0] . PHP_EOL;
+
+
+	$doc->addField($fileNameField);
+	$doc->addField($titleField);
+	$index->addDocument($doc);
+}
