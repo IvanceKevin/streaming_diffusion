aa65cde8ecb1700047afa5c998200772f4295a8a
Bino <ivance.kevin@gmail.com>
2015-04-16 23:50:25 +0200

    RDF

---
+++ web/uploads/test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+
+__author__ = 'ivance kevin & briton paul'
+
+
+from libxmp import *
+from libxmp.core import XMPIterator, XMPMeta
+from libxmp.consts import *
+import libxmp
+import os
+
+	name=f.split('.', 1 )[0];
+	# Read file
+	xmpfile = XMPFiles( file_path="./GypsyPno.wav", open_forupdate=True );
+
+	# Get XMP from file.
+	xmp = xmpfile.get_xmp();
+
+	print xmp;
+
+
